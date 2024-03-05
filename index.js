a5c505f8e5bd71dce7d5d8f4ed2d8377fe5d6ec8
Franchesca <sofia.franchesca2002@gmail.com>
2024-03-05 14:44:11 -0500

    array con secuencia numerica

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ let arr =[];
 let array =["frutas" ,"manzana", "fresa"];
 let mascota =["perro",false,5];
 let objecto = new array("casa","papel","lapiz","libro"); 
+let papel =array.from([1, 2, 3] , (x) => x + x);
