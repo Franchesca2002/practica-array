b1e78b44efcc8fc567f836a7389b85dfffb758ea
Franchesca <sofia.franchesca2002@gmail.com>
2024-03-05 18:57:13 -0500

    array utilizando el constructor

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let arr =[];
 let array =["frutas" ,"manzana", "fresa"];
 let mascota =["perro",false,5];
-let objecto = new array("casa","papel","lapiz","libro"); 
-let papel =array.from([1, 2, 3] , (x) => x + x);
+let objecto = ("casa","papel","lapiz","libro"); 
+let papel =([1, 2, 3] , (x) => x + x);
 let tijera =[1, 2, 3].fill(4); // [4, 4, 4];
-let lapiz = string.split(' ') // (1) [ 'pollito', 'gato' ]
+let lapiz = (' ') // (1) [ 'pollito', 'gato' ]
 let numbers =[0,1];
