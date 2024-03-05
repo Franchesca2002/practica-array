293404f09920cad07034c828bc9869ce45160486
Franchesca <sofia.franchesca2002@gmail.com>
2024-03-05 18:14:30 -0500

    array cadena de texto

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ let mascota =["perro",false,5];
 let objecto = new array("casa","papel","lapiz","libro"); 
 let papel =array.from([1, 2, 3] , (x) => x + x);
 let tijera =[1, 2, 3].fill(4); // [4, 4, 4];
+let lapiz = string.split(' ') // (1) [ 'pollito', 'gato' ]
