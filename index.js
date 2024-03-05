7f9c1d4c33281d3198d28e0446dfa0957348f2f0
Franchesca <sofia.franchesca2002@gmail.com>
2024-03-05 18:31:28 -0500

    array combinando dos arrays

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ let objecto = new array("casa","papel","lapiz","libro");
 let papel =array.from([1, 2, 3] , (x) => x + x);
 let tijera =[1, 2, 3].fill(4); // [4, 4, 4];
 let lapiz = string.split(' ') // (1) [ 'pollito', 'gato' ]
+let numbers =[0,1];
