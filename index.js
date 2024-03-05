79960e8912a3f56697afb2f41abe74203abf15bb
Franchesca <sofia.franchesca2002@gmail.com>
2024-03-05 17:38:00 -0500

    array repeticion de valor

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let array =["frutas" ,"manzana", "fresa"];
 let mascota =["perro",false,5];
 let objecto = new array("casa","papel","lapiz","libro"); 
 let papel =array.from([1, 2, 3] , (x) => x + x);
+let tijera =[1, 2, 3].fill(4); // [4, 4, 4];
