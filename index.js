f42b6a4f64c4a6b070d4ab895c7a8fcc4ff980d9
Franchesca <sofia.franchesca2002@gmail.com>
2024-03-06 00:19:56 -0500

    array eliminacion de elementos

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const colores = ["rojo", "verde", "azul"]; 
-const nuevaLongitud = colores.push("amarillo"); 
-console.log(colores); // ["rojo", "verde", "azul", "amarillo"] 
-console.log(nuevaLongitud); // 4
-
 const frutas = ["manzana", "banana", "naranja"];
  const ultimaFruta = frutas.pop(); 
 console.log(frutas); // ["manzana", "banana"] 
