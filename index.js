cb622ea574a2fb96549f01cac4f5303749bee7ff
Franchesca <sofia.franchesca2002@gmail.com>
2024-03-05 23:24:01 -0500

    longitud de un array

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-function generarTablas (matriz) {
-    return Array.from(matriz, (a,e) => {
-        let tabla = new Array(10)
-        for (let i = 0; i < 10; i++) {
-            tabla[i] = a * (i + 1)
-        }
-        return tabla
-    })
-}
-console.log(generarTablas([1,5,10]))
+let ropa =['zapatos', 'camisas', 'calcetines', 'sueteres'];
+console.log(ropa.longitud)
