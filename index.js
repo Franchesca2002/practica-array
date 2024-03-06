ba7e142c0957485aeb9adc204df94a17a32db89f
Franchesca <sofia.franchesca2002@gmail.com>
2024-03-06 01:21:09 -0500

    subseccion del array

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let palabritas =["se", "da", "casa", "libro"];
-let resultado = palabritas.join("-");
+var a=['a','e','i','o','u'];
+a.slice(1,3); // Devuelve ["e", "i"]
+a.slice(3);  // Devuelve ["o", "u"]
+a.slice(-4,-1); // Devuelve ["e", "i", "o"]
 
 
 
