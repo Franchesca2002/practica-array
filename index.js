d3db83251976459b198fbb25183664da5900a98f
Franchesca <sofia.franchesca2002@gmail.com>
2024-03-06 01:38:19 -0500

    multiples elementos

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var nombres = ["Rita", "Pedro", "Miguel", "Ana", "Vanesa"];
-var masculinos = nombres.slice(1, 3);
+var arr = [1, 2];
 
-// masculinos contiene ['Pedro','Miguel']
+arr.unshift(0); // resultado de la llamada es 3, la nueva longitud del array
+// arr es [0, 1, 2]
+
+arr.unshift(-2, -1); // = 5
+// arr es [-2, -1, 0, 1, 2]
+
+arr.unshift([-3]);
+// arr es [[-3], -2, -1, 0, 1, 2]
 
 
 
