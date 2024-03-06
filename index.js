2af71d7ce6b4e5b749f57df4a9f50038b3c1eeb0
Franchesca <sofia.franchesca2002@gmail.com>
2024-03-05 22:53:19 -0500

    valores dinamicos

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-let numeritos =[1, 2, 2, 3, 4 ,4, 5];
-let final =new Set(numeritos);
-console.log(final)
+function generarTablas (matriz) {
+    return Array.from(matriz, (a,e) => {
+        let tabla = new Array(10)
+        for (let i = 0; i < 10; i++) {
+            tabla[i] = a * (i + 1)
+        }
+        return tabla
+    })
+}
+console.log(generarTablas([1,5,10]))
